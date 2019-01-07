df7cc617c7237a596862c3b4dc747d24aa0e484e
nils govaerts <Nils.govaerts@gmail.com>
2019-01-07 14:56:42 +0100

    Add files via upload

---
+++ dats24/js/script.js
@@ -0,0 +1,10 @@
+let mainNav = document.getElementById('js-nav');
+
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+
+navBarToggle.addEventListener('click', function () {
+    
+    mainNav.classList.toggle('active');
+
+});
