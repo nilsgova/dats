910d9b9889ef00dba95e19fbaccc13f898fecd2e
nilsgova <nils.govaerts@gmail.com>
2019-01-07 21:19:18 +0100

    addes more sections

--- dats24/js/script.js
+++ dats24/js/script.js
@@ -1,10 +1,33 @@
+// navbar toggle
+
 let mainNav = document.getElementById('js-nav');
 
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
 
+
 navBarToggle.addEventListener('click', function () {
-    
+
     mainNav.classList.toggle('active');
 
 });
+
+
+
+// view more
+
+function readMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("card-btn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Leer Meer";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Lees Minder";
+        moreText.style.display = "inline";
+    }
+}
